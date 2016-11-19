a8f73a073096895367522f1effdb267f6f344178
Mehul Kasliwal <Mehul Kasliwal>
2016-11-19 17:47:33 +0530

    Iniital Commit

---
+++ app/src/main/java/com/example/mehul/prevacation/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mehul.prevacation;
+
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    b1= (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse("http://netdna.webdesignerdepot.com/uploads/2013/08/batman.jpg"))
+                        .setTitle("Lorem ipsum")
+                        .setDescription("Hello")
+                        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                downloadManager.enqueue(request);
+
+
+            }
+        });
+
+
+    }
+}
